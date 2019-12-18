c0f1a1c45a1279e41ec4261092cccc979e833713
Ben Lardie <benlardie@gmail.com>
2019-12-17 23:24:44 -0500

    circle follows mouse still needs a few touches, getting closer to link hover effect and other stuff

--- js/main.js
+++ js/main.js
@@ -11,6 +11,11 @@ function myFunction() {
 }
 
 window.onscroll = () => {
-  console.log('scrolling')
   myFunction();
 };
+
+const cursor = document.querySelector('.cursor')
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', `top: ${e.pageY}px; left:${e.pageX}px;`)
+})
