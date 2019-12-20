7358926ecfe889ca9652f747790f173ce94313ce
Ben Lardie <benlardie@gmail.com>
2019-12-20 14:27:51 -0500

    custom curosor is done

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 // loader
 
 window.addEventListener('load', () => {
-    const loader = document.querySelector('.loader')
-    console.log('loading')
+  const loader = document.querySelector('.loader');
+  console.log('loading');
   loader.className += ' hidden';
 });
 
 // progress bar
 
 const progressBar = () => {
-  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const winScroll =
+    document.body.scrollTop || document.documentElement.scrollTop;
   const height =
     document.documentElement.scrollHeight -
     document.documentElement.clientHeight;
   const scrolled = (winScroll / height) * 100;
   document.getElementById('status-bar').style.height = `${scrolled}%`;
-}
+};
 
 window.onscroll = () => {
   progressBar();
@@ -23,7 +24,7 @@ window.onscroll = () => {
 
 // mouse effects
 
-const cursor = document.querySelector('.cursor')
+const cursor = document.querySelector('.cursor');
 const links = document.querySelectorAll('a');
 
 // links.forEach(link => {
@@ -34,27 +35,23 @@ const links = document.querySelectorAll('a');
 // })
 
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute('style', `top: ${e.pageY}px; left:${e.pageX}px;`)
-})
+  cursor.setAttribute('style', `top: ${e.pageY + 12}px; left:${e.pageX + 12}px;`);
+});
 
 // footer
 
+const footer = document.querySelector('footer');
 
-const footer = document.querySelector('footer')
-
-window.addEventListener('scroll', () => { 
-    footer.style.visibility = 'hidden';
+window.addEventListener('scroll', () => {
+  footer.style.visibility = 'hidden';
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
     footer.style.visibility = 'visible';
     footer.style.position = 'fixed';
-    // footer.style.removeProperty('top');
     footer.style.bottom = 0;
-    console.log('bottom')
   }
   if (window.scrollY === 0) {
     footer.style.visibility = 'visible';
     footer.style.top = '748px';
     footer.style.bottom = '';
-    console.log('top')
   }
 });
