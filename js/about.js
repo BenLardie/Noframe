aaef9d816d1adbd37ab07a44c0f673068b32d2d3
Ben Lardie <benlardie@gmail.com>
2020-01-06 23:35:06 -0500

    FIXED OVERFLOW PROBLEM THAT DROVE ME NUTS

--- js/about.js
+++ js/about.js
@@ -1,47 +1,27 @@
-const circle = document.getElementById('circle');
+const circleOne = document.getElementById('circle-one');
+const circleTwo = document.getElementById('circle-two');
 
 const debounce = (func, wait = 20, immediate = true) => {
   let timeout;
   return function() {
-    let context = this;
+    const context = this;
     const args = arguments;
-      const later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      let callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+    const later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
   };
+};
 
-  const spin = () => {
-      console.log('spin')
-    circle.style.transform = `rotate(${window.pageYOffset/2}deg)`;
-  }
-  
+const spin = () => {
+  circleOne.style.transform = `rotate(${window.pageYOffset/5}deg)`;
+  circleTwo.style.transform = `rotate(${window.pageYOffset/5}deg)`;
+};
 
 window.addEventListener('scroll', () => {
   debounce(spin());
-})
-
-// const spin = () => {
-    // const winScroll =
-    //   document.body.scrollTop || document.documentElement.scrollTop;
-    // const height =
-    //   document.documentElement.scrollHeight -
-    //   document.documentElement.clientHeight;
-//   const scrolled = (window.screenTop / 10) % Math.PI;
-//     console.log(scrolled)
-//     circle.style.transform = `rotate(${scrolled}rad)`;
-//   };
-
-//   window.onscroll = () => {
-//     spin();
-//   };
-
-//   $(window).scroll(function() {
-//     var theta = $(window).scrollTop() / 10 % Math.PI;
-//     $('#leftgear').css({ transform: 'rotate(' + theta + 'rad)' });
-//     });
+});
