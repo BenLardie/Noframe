b2399804f978ef18beb6f0c627b5b168edf10e98
Ben Lardie <benlardie@gmail.com>
2020-01-12 15:58:43 -0500

    nav changing based on scroll and have some mouse effects

--- js/nflab.js
+++ js/nflab.js
@@ -23,17 +23,55 @@ window.onscroll = () => {
 };
 
 // mouse
+
+
+const cursorCircle = document.querySelector('.cursor-circle');
 const cursor = document.querySelector('.cursor');
 const links = document.querySelectorAll('a');
 
 document.addEventListener('mousemove', e => {
-  cursor.setAttribute(
+  cursorCircle.setAttribute(
     'style',
     `top: ${e.pageY + 12}px; left:${e.pageX + 12}px;`
   );
 });
 
+window.addEventListener('wheel', function(event)
+{
+ if (event.deltaY < 0)
+ {
+  cursor.innerHTML = '☝';
+ }
+ else if (event.deltaY > 0)
+ {
+    cursor.innerHTML = '☟';
+ }
+});
+
+// nav 
+
+const logo = document.querySelector('.logo');
+const homeLink = document.querySelector('.home-link');
+const article = document.querySelector('article');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY === 0) {
+        homeLink.style.right = '50%';
+    } if (window.scrollY >= article.offsetTop) {
+        logo.style.visibility = 'hidden'
+        homeLink.innerHTML = '^';
+        homeLink.style.fontSize = '50px';
+    } else {
+    homeLink.style.right = '2%';
+    homeLink.innerHTML = 'Back to home';
+    homeLink.style.fontSize = '16px';
+    logo.style.visibility = 'visible';
+}
+})
+
+
 // wayfinder
+
 window.addEventListener('DOMContentLoaded', () => {
   const blogLinks = document.querySelectorAll('.way-link');
   const boxes = document.querySelectorAll('.box');
@@ -41,7 +79,9 @@ window.addEventListener('DOMContentLoaded', () => {
   blogLinks.forEach(link => {
     link.addEventListener('mouseover', e => {
       const box = boxes[e.target.dataset.month - 1];
+      link.style.cursor = 'none';
       box.style.opacity = 0;
+      cursor.innerHTML = '✹'
     });
   });
 
@@ -50,6 +90,7 @@ window.addEventListener('DOMContentLoaded', () => {
       const box = boxes[e.target.dataset.month - 1];
       box.style.opacity = 1;
       box.style.background = 'rgba(255, 255, 255, 0.5)';
+      cursor.innerHTML = '☟';
     });
   });
 });
